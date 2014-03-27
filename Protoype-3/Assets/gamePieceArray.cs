4602aca564a6bd9e52a65eb342dee58418ce071b
Josh Sarath <joshua.sarath@gmail.com>
2014-03-27 19:01:29 -0400

    update

--- Protoype-3/Assets/gamePieceArray.cs
+++ Protoype-3/Assets/gamePieceArray.cs
@@ -2,15 +2,15 @@
 using System.Collections;
 
 public class gamePieceArray : MonoBehaviour {
-	public int[] piecesArray;// 0-3 top row, etc
+	//public int[] piecesArray;// 0-3 top row, etc
 	//0 is empty, 1 is pawn, 3 is bishop, 6 is knight, 9 is rook, 12 is queen, 15 is king 
 
 	// Use this for initialization
 	void Start () {
-		for(int i=0;i<4;i++){
+		/*for(int i=0;i<4;i++){
 			piecesArray[i]=0;
 		}
-		Debug.Log(piecesArray);
+		Debug.Log(piecesArray);*/
 	}
 	
 	// Update is called once per frame
