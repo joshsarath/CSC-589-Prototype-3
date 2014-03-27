44b70c2b4e7e6cc7a4480afa1431caebf44af1b8
Josh Sarath <joshua.sarath@gmail.com>
2014-03-27 01:08:47 -0400

    Started project
    Have movement working on one item, still working on getting an array to
    work

---
+++ Protoype-3/Assets/Movement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	private float counter=0;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter += Time.deltaTime;
+		//gamePieceArray array = GameObject.Find("Grid").GetComponent<gamePieceArray>();
+		if (counter >0.1f){
+			if (Input.GetKey("up")){
+				if (transform.position.y<=4.5){
+					//array.piecesArray[1]=1;
+					transform.position=new Vector3(transform.position.x,6.25f, 3);
+					counter=0;
+				}
+			}
+			if (Input.GetKey("down")){
+				if (transform.position.y>=-2.5){
+					transform.position=new Vector3(transform.position.x,-4.25f,3);
+					counter=0;
+				}
+			}
+			if (Input.GetKey("left")){
+				if (transform.position.x>=-3.5){
+					transform.position=new Vector3(-5.25f,transform.position.y,3);
+					counter=0;
+				}
+			}
+			if (Input.GetKey("right")){
+				if (transform.position.x<=3.5){
+					transform.position=new Vector3(5.25f,transform.position.y,3);
+					counter=0;
+				}
+			}
+		}
+	}
+}
---
+++ Protoype-3/Assets/gamePieceArray.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gamePieceArray : MonoBehaviour {
+	public int[] piecesArray;// 0-3 top row, etc
+	//0 is empty, 1 is pawn, 3 is bishop, 6 is knight, 9 is rook, 12 is queen, 15 is king 
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0;i<4;i++){
+			piecesArray[i]=0;
+		}
+		Debug.Log(piecesArray);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
