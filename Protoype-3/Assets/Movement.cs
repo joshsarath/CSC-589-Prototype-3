8d6af330c48c82f475a170afc5d5babadceb7992
Josh Sarath <joshua.sarath@gmail.com>
2014-03-29 15:45:43 -0400

    changed grid layout

--- Protoype-3/Assets/Movement.cs
+++ Protoype-3/Assets/Movement.cs
@@ -16,30 +16,57 @@ public class Movement : MonoBehaviour {
 		//gamePieceArray array = GameObject.Find("Grid").GetComponent<gamePieceArray>();
 		if (counter >0.1f){
 			if (Input.GetKey("up")){
-				if (transform.position.y<=4.5){
-					//array.piecesArray[1]=1;
-					transform.position=new Vector3(transform.position.x,6.25f, 3);
+				//if (transform.position.y<=4.5){
+					transform.position=new Vector3(transform.position.x,11.75f, transform.position.z);
 					counter=0;
-				}
+					//updateArray();
+				//}
 			}
 			if (Input.GetKey("down")){
 				if (transform.position.y>=-2.5){
-					transform.position=new Vector3(transform.position.x,-4.25f,3);
+					transform.position=new Vector3(transform.position.x,1.25f,transform.position.z);
 					counter=0;
 				}
 			}
 			if (Input.GetKey("left")){
 				if (transform.position.x>=-3.5){
-					transform.position=new Vector3(-5.25f,transform.position.y,3);
+					transform.position=new Vector3(-5.25f,transform.position.y,transform.position.z);
 					counter=0;
 				}
 			}
 			if (Input.GetKey("right")){
 				if (transform.position.x<=3.5){
-					transform.position=new Vector3(5.25f,transform.position.y,3);
+					transform.position=new Vector3(5.25f,transform.position.y,transform.position.z);
 					counter=0;
 				}
 			}
 		}
 	}
+	void updateArray(int vert, int hort){
+		gamePieceArray grid = GameObject.Find("Grid").GetComponent<gamePieceArray>();
+		grid.array [hort, vert] = 1;
+		Debug.Log ("This worked");
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Protoype-3/Assets/gamePieceArray.cs
+++ Protoype-3/Assets/gamePieceArray.cs
@@ -2,17 +2,20 @@
 using System.Collections;
 
 public class gamePieceArray : MonoBehaviour {
-	//public int[] piecesArray;// 0-3 top row, etc
+	public int[,] array= new int[4,4];
+	//public int[,] array;// 0-3 top row, etc
 	//0 is empty, 1 is pawn, 3 is bishop, 6 is knight, 9 is rook, 12 is queen, 15 is king 
 
 	// Use this for initialization
 	void Start () {
-		/*for(int i=0;i<4;i++){
-			piecesArray[i]=0;
+
+			for(int i=0; i<4; i++){
+				for(int j=0; j<4; j++){
+					array[i,j]=0;
+				}
+			}
 		}
-		Debug.Log(piecesArray);*/
-	}
-	
+			
 	// Update is called once per frame
 	void Update () {
 
